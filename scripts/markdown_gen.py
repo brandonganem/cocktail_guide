62a41fccb388db1336411cf24b6b901935cb47ea
Brandon Ganem <brandonganem@github.com>
2021-05-18 00:09:56 -0400

    basic script for rendering yaml down to markdown

---
+++ scripts/markdown_gen.py
@@ -0,0 +1,45 @@
+import yaml
+import os
+import re
+import glob
+from jinja2 import Environment, FileSystemLoader
+
+def _short_desc(style):
+    if re.match('(?i)^[a,e,i,o,u]', style):
+        return f"An _{style}_ Cocktail."
+    return f"A {style} Cocktail."
+
+def _generate_ingredients(ingredients):
+    return_val = []
+    for ingredient in ingredients:
+        return_val.append({"name": ingredient, "pour": ingredients[ingredient]})
+    return return_val
+
+def load_cocktails(path):
+    cocktail_yamls = glob.iglob(f'{path}/**/*.yaml', recursive=True)
+    cocktails = []
+    for cocktail in cocktail_yamls:
+        with open(cocktail, 'r') as f:
+            try:
+                cocktail_dict = yaml.safe_load(f)
+                cocktail_dict['cocktail_desc_short'] = _short_desc(cocktail_dict['Style'])
+                cocktail_dict['Ingredients'] =_generate_ingredients(cocktail_dict['Ingredients'])
+                cocktails.append(cocktail_dict)
+            except yaml.YAMLError as exc:
+                print(exc)
+                raise("Unable to load cocktail.")
+    return cocktails
+
+def main():
+
+    j2_env = Environment(loader=FileSystemLoader('.'), trim_blocks=True)
+    template = j2_env.get_template('cocktail.jinja2')
+    if not os.path.exists('../pages'):
+        os.makedirs('../pages')
+    for cocktail in load_cocktails("../cocktails"):
+        out = template.render(cocktail)
+        with open(f"../pages/{cocktail['Name']}.md", "w") as f:
+            f.write(out)
+
+if __name__ == "__main__":
+    main()
